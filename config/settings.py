06040aa69026a15e8edfb86dd2c7d2529326269e
FarmonovH <92896072+FarmonovH@users.noreply.github.com>
2022-06-19 17:54:49 +0500

    heroku uchun ozgartirishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd5k*4vu=y4oe69n&pc+yb8_9ymg18@a7=zd&ug^b-9y23t79y('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
